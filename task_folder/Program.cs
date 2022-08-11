52a6d5e7e55b015e16da544ffaa301a211e41c8c
Kirill <ForStmKar@gmail.com>
2022-08-11 18:36:05 +0300

    readarray and printarray methods added

--- task_folder/Program.cs
+++ task_folder/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] words= new string[5];
+
+void ReadArray(string[] array)
+{
+    for( int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine(array[i]);
+    }
+}
+ReadArray(words);
+Console.WriteLine();
+PrintArray(words);
